1e991514ee38e8bed5ea113cb1febbf975dab5f3
Gal <galm85@gmail.com>
2025-09-15 12:13:51 +0300

    create header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule,FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+
+  isUserMenuOpen:boolean = false;
+
+
+  toggleUserMenu(){
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+
+  onSearch(searchValue:string){
+    // TODO: Implements filters
+    console.log('search value: ',searchValue);
+  }
 
 }
